d5e8301d00db6535d559efc1420441e94daa947c
hehongyi0217 <hehongyi0217@gmail.com>
2024-01-30 14:05:32 +0800

    [modify] intake

--- src/main/java/frc/robot/Subsystem/IntakeSubsystem.java
+++ src/main/java/frc/robot/Subsystem/IntakeSubsystem.java
@@ -13,28 +13,55 @@ public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new Intake. */
   private final VictorSP intakeMotor1;
   private final VictorSP intakeMotor2;
+  private double setIntakeMotor1age = 0.0;
+  private double setIntakeMotor2age = 0.0;
+  private boolean intakecondition = true;
 
   public IntakeSubsystem() {
     intakeMotor1 = new VictorSP(IntakeConstants.kintakeonePWMID);
     intakeMotor2 = new VictorSP(IntakeConstants.kintaketwoPWMID);
     intakeMotor1.setInverted(IntakeConstants.kintakeoneInvert);
     intakeMotor2.setInverted(IntakeConstants.kintaketwoInvert);
-  
+    SmartDashboard.putNumber("setIntakeMotor1Voltage", setIntakeMotor1age);
+    SmartDashboard.putNumber("setIntakeMotor2Voltage", setIntakeMotor2age);
 
   }
 
   public void setVoltage() {
-    intakeMotor1.setVoltage(4);
-    intakeMotor2.setVoltage(4);
+    intakeMotor1.setVoltage(SmartDashboard.getNumber("setIntakeMotor1Voltage", 0));
+    intakeMotor2.setVoltage(SmartDashboard.getNumber("setIntakeMotor2Voltage", 0));
   }
 
-  public void stopVoltage(){
+  public void stopVoltage() {
     intakeMotor1.setVoltage(0);
     intakeMotor2.setVoltage(0);
+  }
+
+  public void setIntakecondition() {
+    if (intakecondition) {
+      setVoltage();
+    } else {
+      stopVoltage();
+    }
+    intakecondition = !intakecondition;
+  }
+
+  public void getDashboard() {
+    SmartDashboard.getNumber("setIntakeMotor1Voltage", 0.0);
+    SmartDashboard.getNumber("setIntakeMotor2Voltage", 0.0);
+
+  }
+
+  public void putDashboard() {
+    SmartDashboard.putNumber("out_putsetIntakeMotor1Voltage", intakeMotor1.get() * 12.0);
+    SmartDashboard.putNumber("out_putsetIntakeMotor2Voltage", intakeMotor2.get() * 12.0);
+
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  } 
+    putDashboard();
+    getDashboard();
+  }
 }
