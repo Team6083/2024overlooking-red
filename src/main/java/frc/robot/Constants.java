02060cbdd0d0eafbea7e24108956a38bf37b963f
Lisacyj0102 <s1110169@my.cmsh.cyc.edu.tw>
2024-02-04 09:59:06 +0800

    [mod] DriveBase

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,9 +14,39 @@ public final class Constants {
         public static final Boolean kintakeoneInvert = false;
         public static final Boolean kintaketwoInvert = false;
     }
-    public static class RiseShooterConstants{
-        public static final int kRiseShooterPWMID=11;
+
+    public static class RiseShooterConstants {
+        public static final int kRiseShooterPWMID = 11;
         public static final Boolean kRiseShooterInvert = false;
 
+    }
+
+    public static class DrivebaseConstants {
+        public static final double kMaxSpeed = 5;
+        public static final double kMinSpeed = 0.25;
+        public static final double kMinJoyStickValue = 0.3;
+        public static final double kMaxAngularSpeed = 2.5 * Math.PI;
+
+        public static final double xLimiterRateLimit = 3.0;
+        public static final double yLimiterRateLimit = 3.0;
+        public static final double rotLimiterRateLimit = 3.0;
+
+        public static final boolean kFrontLeftDriveMotorInverted = false;
+        public static final boolean kFrontRightDriveMotorInverted = false;
+        public static final boolean kBackLeftDriveMotorInverted = false;
+        public static final boolean kBackRightDriveMotorInverted = false;
+    }
+
+    public static final class ModuleConstants {
+        public static final double kWheelRadius = 0.046;
+        public static final double kWheelDiameterMeters = 0.15;
+        public static final double kLimitModuleDriveVoltage = 7.0;
+        public static final double kMaxModuleDriveVoltage = 12.0;
+        public static final double kClosedLoopRampRate = 0.25;// 1 second 1 unit
+        public static final double kDesireSpeedtoMotorVoltage = kMaxModuleDriveVoltage / DrivebaseConstants.kMaxSpeed;
+        public static final double kMaxModuleTuringVoltage = 7.0;
+        public static final double kMaxSpeedTurningDegree = 180.0;
+        public static final double kPRotController = kMaxModuleTuringVoltage / kMaxSpeedTurningDegree;
+        public static final double kDRotController = 0.0004;
     }
 }
