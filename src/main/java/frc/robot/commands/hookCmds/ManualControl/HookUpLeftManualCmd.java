72f68239be35c204f6cd7361382263176ca0aca8
hehongyi0217 <hehongyi0217@gmail.com>
2024-03-01 20:45:33 +0800

    [fix]hook  manual

--- src/main/java/frc/robot/commands/hookCmds/ManualControl/HookUpLeftManualCmd.java
+++ src/main/java/frc/robot/commands/hookCmds/ManualControl/HookUpLeftManualCmd.java
@@ -38,6 +38,6 @@ public class HookUpLeftManualCmd extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
--- src/main/java/frc/robot/commands/hookCmds/ManualControl/HookUpRightManualCmd.java
+++ src/main/java/frc/robot/commands/hookCmds/ManualControl/HookUpRightManualCmd.java
@@ -39,6 +39,6 @@ public class HookUpRightManualCmd extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
