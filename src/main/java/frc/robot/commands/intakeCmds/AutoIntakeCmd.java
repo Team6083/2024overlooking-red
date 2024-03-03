802f504530b4c376d4709e0706a8348e292fa761
林柏宇 <117905295+William960321@users.noreply.github.com>
2024-03-03 09:32:45 +0800

    [mov] all intake cmds to folder

--- src/main/java/frc/robot/commands/intakeCmds/AutoIntakeCmd.java
+++ src/main/java/frc/robot/commands/intakeCmds/AutoIntakeCmd.java
@@ -2,44 +2,43 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.intakeCmds;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class TeleopIntakeCmd extends Command {
-  /** Creates a new TeleopModeIntakeCmd. */
-  private final IntakeSubsystem intakeSubsystem;
-  private final boolean isGetNote;
+public class AutoIntakeCmd extends Command {
+  /** Creates a new StartIntake. */
+  private final IntakeSubsystem intake;
 
-  public TeleopIntakeCmd(IntakeSubsystem intakeSubsystem, boolean isGetNote) {
-    this.intakeSubsystem = intakeSubsystem;
-    this.isGetNote = isGetNote;
-    addRequirements(this.intakeSubsystem);
+  public AutoIntakeCmd(IntakeSubsystem intake) {
+    this.intake = intake;
+    addRequirements(this.intake);
+
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    intakeSubsystem.stopMotor();
+    intake.stopMotor();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intakeSubsystem.setIntaking();
+    intake.setIntaking();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intakeSubsystem.stopMotor();
+    intake.stopMotor();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return isGetNote;
+    return false;
   }
 }
--- src/main/java/frc/robot/commands/intakeCmds/TeleopIntakeCmd.java
+++ src/main/java/frc/robot/commands/intakeCmds/TeleopIntakeCmd.java
@@ -2,43 +2,44 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.intakeCmds;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class AutoIntakeCmd extends Command {
-  /** Creates a new StartIntake. */
-  private final IntakeSubsystem intake;
+public class TeleopIntakeCmd extends Command {
+  /** Creates a new TeleopModeIntakeCmd. */
+  private final IntakeSubsystem intakeSubsystem;
+  private final boolean isGetNote;
 
-  public AutoIntakeCmd(IntakeSubsystem intake) {
-    this.intake = intake;
-    addRequirements(this.intake);
-
+  public TeleopIntakeCmd(IntakeSubsystem intakeSubsystem, boolean isGetNote) {
+    this.intakeSubsystem = intakeSubsystem;
+    this.isGetNote = isGetNote;
+    addRequirements(this.intakeSubsystem);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    intake.stopMotor();
+    intakeSubsystem.stopMotor();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intake.setIntaking();
+    intakeSubsystem.setIntaking();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intake.stopMotor();
+    intakeSubsystem.stopMotor();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return isGetNote;
   }
 }
