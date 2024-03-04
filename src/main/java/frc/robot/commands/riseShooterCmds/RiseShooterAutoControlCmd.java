8913e4d877771a8575c0eeb39269560e3c6ed53d
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2024-03-05 02:50:41 +0800

    [add] the riseshooter getSetpoint

--- src/main/java/frc/robot/commands/riseShooterCmds/RiseShooterAutoControlCmd.java
+++ src/main/java/frc/robot/commands/riseShooterCmds/RiseShooterAutoControlCmd.java
@@ -20,7 +20,7 @@ public class RiseShooterAutoControlCmd extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    riseShooterSubsystem.setSetpoint(riseShooterSubsystem.getAprilTagDegree());
+    riseShooterSubsystem.setSetpoint(riseShooterSubsystem.getAprilTagDegree(riseShooterSubsystem.getSetpoint()));
   }
 
   // Called every time the scheduler runs while the command is scheduled.
